16aa46271df2d8458f2a42ee72d0e93be29a2026
wiselook <1656438881@qq.com>
2025-07-09 15:27:05 +0800

    Update gimbal_controller.

--- rm_gimbal_controllers/include/rm_gimbal_controllers/bullet_solver/bullet_solver.h
+++ rm_gimbal_controllers/include/rm_gimbal_controllers/bullet_solver/bullet_solver.h
@@ -46,31 +46,35 @@
 #include <rm_common/hardware_interface/robot_state_interface.h>
 #include <rm_common/eigen_types.h>
 #include <rm_common/ros_utilities.h>
-#include <std_msgs/Bool.h>
-#include <std_msgs/Float64.h>
-#include <rm_msgs/TrackData.h>
-#include <rm_msgs/ShootBeforehandCmd.h>
+#include "rm_gimbal_controllers/bullet_solver/target_kinematics_model.h"
 
 namespace rm_gimbal_controllers
 {
 struct Config
 {
   double resistance_coff_qd_10, resistance_coff_qd_15, resistance_coff_qd_16, resistance_coff_qd_18,
-      resistance_coff_qd_30, g, delay, wait_next_armor_delay, wait_diagonal_armor_delay, dt, timeout,
-      ban_shoot_duration, gimbal_switch_duration, max_switch_angle, min_switch_angle, min_shoot_beforehand_vel,
-      max_chassis_angular_vel, track_rotate_target_delay, track_move_target_delay;
-  int min_fit_switch_count;
+      resistance_coff_qd_30, g, delay, dt, timeout;
+};
+
+enum class SelectedArmor
+{
+  FRONT = 0,
+  LEFT = 1,
+  BACK = 2,
+  RIGHT = 3
 };
 
 class BulletSolver
 {
 public:
   explicit BulletSolver(ros::NodeHandle& controller_nh);
-
-  bool solve(geometry_msgs::Point pos, geometry_msgs::Vector3 vel, double bullet_speed, double yaw, double v_yaw,
-             double r1, double r2, double dz, int armors_num, double chassis_angular_vel_z);
-  double getGimbalError(geometry_msgs::Point pos, geometry_msgs::Vector3 vel, double yaw, double v_yaw, double r1,
-                        double r2, double dz, int armors_num, double yaw_real, double pitch_real, double bullet_speed);
+  // normal target(including robots and buildings)
+  void input(geometry_msgs::Point pos, geometry_msgs::Vector3 vel, double bullet_speed, double yaw, double v_yaw,
+             double r1, double r2, double dz, int armors_num);
+  // windmill
+  void input(double theta, double theta_dot, double bullet_speed, geometry_msgs::TransformStamped windmill2odom);
+  bool solve();
+  double getGimbalError(double yaw_real, double pitch_real);
   double getResistanceCoefficient(double bullet_speed) const;
   double getYaw() const
   {
@@ -80,39 +84,31 @@ public:
   {
     return -output_pitch_;
   }
-  void getSelectedArmorPosAndVel(geometry_msgs::Point& armor_pos, geometry_msgs::Vector3& armor_vel,
-                                 geometry_msgs::Point pos, geometry_msgs::Vector3 vel, double yaw, double v_yaw,
-                                 double r1, double r2, double dz, int armors_num);
-  void judgeShootBeforehand(const ros::Time& time, double v_yaw);
+  void getYawVelAndAccelDes(double& vel_des, double& accel_des);
+  void getPitchVelAndAccelDes(double& vel_des, double& accel_des);
   void bulletModelPub(const geometry_msgs::TransformStamped& odom2pitch, const ros::Time& time);
-  void identifiedTargetChangeCB(const std_msgs::BoolConstPtr& msg);
   void reconfigCB(rm_gimbal_controllers::BulletSolverConfig& config, uint32_t);
   ~BulletSolver() = default;
 
 private:
   std::shared_ptr<realtime_tools::RealtimePublisher<visualization_msgs::Marker>> path_desire_pub_;
   std::shared_ptr<realtime_tools::RealtimePublisher<visualization_msgs::Marker>> path_real_pub_;
-  std::shared_ptr<realtime_tools::RealtimePublisher<rm_msgs::ShootBeforehandCmd>> shoot_beforehand_cmd_pub_;
-  std::shared_ptr<realtime_tools::RealtimePublisher<std_msgs::Float64>> fly_time_pub_;
-  ros::Subscriber identified_target_change_sub_;
-  ros::Time switch_armor_time_{};
   realtime_tools::RealtimeBuffer<Config> config_rt_buffer_;
   dynamic_reconfigure::Server<rm_gimbal_controllers::BulletSolverConfig>* d_srv_{};
   Config config_{};
   double max_track_target_vel_;
-  double output_yaw_{}, output_pitch_{};
-  double bullet_speed_{}, resistance_coff_{};
-  double fly_time_;
-  double switch_hysteresis_;
-  int shoot_beforehand_cmd_{};
-  int selected_armor_;
-  int count_;
-  bool track_target_ = true;
-  bool identified_target_change_ = true;
-  bool is_in_delay_before_switch_{};
   bool dynamic_reconfig_initialized_{};
+  double output_yaw_{}, output_pitch_{};
+  double last_pitch_vel_des_{};
+  ros::Time last_pitch_vel_des_solve_time_{ 0 };
+  double bullet_speed_{}, resistance_coff_{};
+  double windmill_radius_;
+  SelectedArmor selected_armor_;
+  bool track_target_;
 
+  std::shared_ptr<TargetKinematicsBase> target_kinematics_;
   geometry_msgs::Point target_pos_{};
+  double fly_time_;
   visualization_msgs::Marker marker_desire_;
   visualization_msgs::Marker marker_real_;
 };
